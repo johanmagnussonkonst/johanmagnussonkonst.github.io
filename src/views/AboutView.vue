bf8f33ead8ec78faa27df64f7cc2994a0aac3b00
Unstriped <stefanmagnusson90@gmail.com>
2023-03-11 11:52:39 +0100

    updated CV

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -12,70 +12,59 @@
 
     <h2 class="text-xl">Biografi</h2>
     <ul class="text-md mb-2">
-      <li>Född 1954 i Järfälla</li>
-      <li>Verksam i Norrköping sedan 2022</li>
-    </ul>
-
-    <h2 class="text-xl">Konstnärlig utbildning</h2>
-    <ul class="text-md mb-2">
-      <li>Gerlesborgsskolan i Stockholm, 1984-86</li>
-      <li>Ölands Folkhögskola, 1983-84</li>
-    </ul>
-
-    <h2 class="text-xl">Kommande Utställningar</h2>
-    <ul class="text-md mb-2">
-      <li>Dagar som jag glömt - Konstfrämjandet Värmland</li>
-      <li>31 mars - 22 april</li>
+      <li>Född 1954 i Järfälla, Stockholm</li>
+      <li>Bor och är verksam i Norrköping sedan 2022</li>
     </ul>
 
     <h2 class="text-xl">Separatutställningar i urval</h2>
     <ul class="text-md mb-2">
+      <li>Dagar som jag glömt, Konstfrämjandet Värmland, 2023</li>
       <li>Galleri Ekdahl, Karlstad, 2022</li>
-      <li>Arvika Konsthall, Östanvinden, 2021</li>
-      <li>Konst i Karlstad, Med Tiden, 2019</li>
+      <li>Östanvinden, Arvika Konsthall, 2021</li>
+      <li>Med Tiden, Konst i Karlstad, 2019</li>
       <li>Galleri Nord, Örebro, 2003</li>
       <li>Arvika Konsthall, 2003</li>
       <li>Galleri Bacchus, Borås, 2002</li>
       <li>Kristinehamns konsthall, 2001</li>
-      <li>Konstfrämjandet, 16 Plåtar, Karlstad, 2001</li>
+      <li>16 Plåtar, Konstfrämjandet, Karlstad, 2001</li>
       <li>Galleri Hera, Stockholm, 2000</li>
       <li>Galleri Nord, Örebro, 1998</li>
       <li>Galleri Gripen, Karlstad, 1998</li>
     </ul>
 
     <h2 class="text-xl">Samlingsutställningar i urval</h2>
-
     <ul class="text-md mb-2">
       <li>Nora Art, Återträda, Bryggeriet, 2020</li>
-      <li>Konstfrämjandet Värmland, Konstvandring, 2020</li>
-      <li>Värmlands Höstsalong, 2001–2020</li>
+      <li>Konstvandring på Herrhagen, Konstfrämjandet Värmland, 2020</li>
+      <li>Värmlands Konstförenings Höstsalong, 2001 – 2020</li>
       <li>Konstruktiv Tendens, Stockholm, 2005</li>
       <li>KonstForum, Norrköping, 2003</li>
-      <li>Värmlands Museum, Det finns ett hemligt tecken, 2003</li>
-      <li>Gotlands konstmuseum, Värmländsk konst, 2001</li>
-      <li>Värmlands Museum, Sverige bjuder till bords, 2000</li>
+      <li>Det finns ett hemligt tecken, Värmlands Museum, 2003</li>
+      <li>Värmländsk konst, Gotlands konstmuseum, 2001</li>
+      <li>Sverige bjuder till bords, Värmlands Museum, 2000</li>
       <li>
-        Lützens Slottsmuseum, Vanitas-Vanitatis, Schweden bittet zu tisch, 1999
+        Vanitas-Vanitatis, Schweden bittet zu tisch, Lützens Slottsmuseum, 1999
       </li>
     </ul>
 
     <h2 class="text-xl">Representerad</h2>
-
     <p class="text-md mb-2">
-      Värmlands Museum, Kristinehamns konstmuseum, Alma Löv Museum Regioner &
-      kommuner
+      Värmlands Museum, Kristinehamns konstmuseum, Alma Löv Museum <br />
+      Region Värmland, Region Västra Götaland, Region Örebro län, Region
+      Gotland, <br />
+      Karlstads kommun, Kristinehamns kommun
     </p>
 
     <h2 class="text-xl">Övrigt</h2>
     <ul class="text-md mb-2">
       <li>Konstintendent, Värmlands Museum, 2019</li>
-      <li>Konsthandläggare, Region Värmland, 2017-2019</li>
+      <li>Konsthandläggare/Inventering, Region Värmland, 2017 - 2019</li>
       <li>
-        Extern rådgivare, Rådet för Konstnärlig Gestaltning i Karlstads Kommun,
-        2017-2021
+        Extern rådgivare i Rådet för Konstnärlig Gestaltning i Karlstads Kommun,
+        2017 - 2021
       </li>
       <li>
-        Konstintendent / Tf. museichef, Kristinehamns konstmuseum, 2002-2013
+        Konstintendent / Tf. museichef, Kristinehamns konstmuseum, 2002 - 2013
       </li>
       <li>
         Assistent till Ann Edholm, Transir, Nya Tingshuset, Helsingborg, Statens
@@ -84,10 +73,6 @@
     </ul>
 
     <h2 class="text-xl">Kontakt</h2>
-    <a
-      class="text-md mb-2 underline terra-link"
-      href="mailto: magnussonjohan6@gmail.com"
-      >magnussonjohan6@gmail.com</a
-    >
+    <p class="text-md mb-2">070-328 82 85</p>
   </div>
 </template>
